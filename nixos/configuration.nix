e49b3e0a3f4999605062de8d88a0276b430545c2
fortuneteller2k <lythe1107@gmail.com>
2020-11-20 23:19:08 +0800

    enable autoupgrades

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -168,6 +168,9 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
+  system = {
+    stateVersion = "20.09"; # Did you read the comment?
+    autoUpgrade.enable = true;
+  };
 }
 
