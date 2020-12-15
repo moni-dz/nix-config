b62b86b89b58cb6a06d878e0b5effaaf5c67ff2a
fortuneteller2k <lythe1107@gmail.com>
2020-12-15 16:47:56 +0800

    remove nixpkgs config from configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,32 +1,11 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, options, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./cachix.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./cachix.nix
+  ];
 
-  # Allow nonfree software
-  nixpkgs = {
-    config.allowUnfree = true;
-    overlays = [
-      (self: super: {
-        picom = super.picom.overrideAttrs (old: {
-          src = super.fetchFromGitHub {
-            owner = "jonaburg";
-            repo = "picom";
-            rev = "a8445684fe18946604848efb73ace9457b29bf80";
-            sha256 = "154s67p3lxdv9is3lnc32j48p7v9n18ga1j8ln1dxcnb38c19rj7";
-          };
-        });
-      })
-    ];
-  };
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''experimental-features = nix-command flakes'';
@@ -39,7 +18,6 @@
     autoOptimiseStore = true;
   };
 
-  # Use the grub EFI boot loader.
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
     kernelParams = [
@@ -58,17 +36,13 @@
     };
   };
  
-  # Set your time zone.
   time.timeZone = "Asia/Manila";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking = {
-    hostName = "nixos"; # Set your hostname
+    hostName = "nixos";
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
     networkmanager = {
-      enable = true; # Enable wireless with NetworkManager
+      enable = true;
       dns = "none";
     };
     useDHCP = false;
@@ -78,8 +52,8 @@
     };
   };
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
@@ -87,7 +61,6 @@
 
   services = {
     xserver = {
-      # Enable selected window managers.
       enable = true;
       displayManager = {
         gdm.enable = true;
@@ -99,9 +72,7 @@
           enableContribAndExtras = true;
         };
       };
-      # Configure keymap in X11
       layout = "us";
-      # Enable touchpad support (enabled default in most desktopManager).
       libinput.enable = true;
     };
     picom = {
@@ -109,18 +80,6 @@
       experimentalBackends = true;
       backend = "glx";
       vSync = true;
-      settings = {
-        transition-length = 150;
-        spawn-center-screen = true;
-        blur = {
-          method = "dual_kawase";
-          strength = 5;
-          background = false;
-          background-frame = false;
-          background-fixed = false;
-        };
-        blur-background-exclude = [ "window_type = 'dock'" "window_type = 'desktop'" ];
-      };
     };
     chrony = {
       enable = true;
@@ -135,11 +94,11 @@
     openssh.enable = true;
   };
 
-  # Enable sound.
   sound.enable = true;
+
   hardware = { 
-    pulseaudio.enable = true; # Use pulseaudio for sound
-    acpilight.enable = true; # Backlight control
+    pulseaudio.enable = true;
+    acpilight.enable = true;
     opengl = {
       enable = true;
       extraPackages = with pkgs; [
@@ -149,19 +108,15 @@
     };
   };
 
-  # sudo needs no password
   security.sudo.wheelNeedsPassword = false;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.fortuneteller2k = {
     isNormalUser = true;
     home = "/home/fortuneteller2k";
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "networkmanager" "video" "audio" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget
     coreutils
@@ -179,6 +134,7 @@
     ripgrep
     fd
     nixfmt
+    python3
     shellcheck
     gcc
     rustup
@@ -198,7 +154,7 @@
   ];
 
   programs = {
-    slock.enable = true; # screen locker
+    slock.enable = true;
     zsh = {
       enable = true;
       syntaxHighlighting.enable = true;
@@ -225,7 +181,6 @@
 
   services.dbus.packages = with pkgs; [ gnome3.dconf ];
   
-  # Font packages and configuration
   fonts = {
     fonts = with pkgs; [
       nerdfonts
@@ -244,14 +199,8 @@
     };
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system = {
-    stateVersion = "20.09"; # Did you read the comment?
+    stateVersion = "20.09";
     autoUpgrade.enable = true;
   };
 }
