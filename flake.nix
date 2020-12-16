82f5cc1abad80d28c04f5a115dcd1f7b421614db
fortuneteller2k <lythe1107@gmail.com>
2020-12-16 20:45:26 +0800

    add nixos-hardware

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   description = "A minimal NixOS configuration using Nix Flakes.";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    };
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     home = {
-      url = "github:nix-community/home-manager";
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
-    nur.url = "github:nix-community/NUR";
+    emacs-overlay.url = github:nix-community/emacs-overlay;
+    nur.url = github:nix-community/NUR;
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
   };
 
   outputs = inputs @ { self, nixpkgs, home, emacs-overlay, nur }: {
