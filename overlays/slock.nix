f13a81706e6bc443d8ecfac816e2db27ca841231
fortuneteller2k <lythe1107@gmail.com>
2020-12-20 23:31:57 +0800

    use local tarball that has patches

--- overlays/slock.nix
+++ overlays/slock.nix
@@ -1,9 +1,5 @@
 final: prev: {
   slock = prev.slock.overrideAttrs (old: {
-    patches = [
-      ./patches/slock-blur_pixelated_screen-1.4.diff
-      ./patches/slock-dpms-1.4.diff
-      ./patches/slock-quickcancel-1.4.diff
-    ];
+    src = "./tarballs/slock.tar.gz";
   });
 }
