c7255cf0fb9ecc0205821341984a10f5d49012b6
fortuneteller2k <lythe1107@gmail.com>
2020-12-20 23:40:41 +0800

    add imlib2 build input

--- overlays/slock.nix
+++ overlays/slock.nix
@@ -1,5 +1,8 @@
 final: prev: {
   slock = prev.slock.overrideAttrs (old: {
-    src = "./tarballs/slock.tar.gz";
+    src = ./tarballs/slock.tar.gz;
+    buildInputs = (old.buildInputs or []) ++ [
+      prev.imlib2
+    ];
   });
 }
