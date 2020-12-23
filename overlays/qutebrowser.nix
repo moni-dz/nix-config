85c6e2a0f91a1414a46a9598637de2b328365a9a
fortuneteller2k <lythe1107@gmail.com>
2020-12-23 17:26:39 +0800

    update hash

--- overlays/qutebrowser.nix
+++ overlays/qutebrowser.nix
@@ -4,7 +4,7 @@ final: prev: {
       owner = "qutebrowser";
       repo = "qutebrowser";
       rev = "db70f34ba3d06d5abe2d0af667cafe0123c80dba";
-      sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "sha256-rW9qzCMDig8RQXAwkNeaXHuzlFsZiFDGwjz3KmsuhHk=";
     };
   });
 }
