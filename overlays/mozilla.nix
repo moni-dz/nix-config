20a08fc4e7b753d2559e3e1a1a9bdf9072fe63a4
fortuneteller2k <lythe1107@gmail.com>
2021-01-14 23:40:16 +0800

    add sha256

--- overlays/mozilla.nix
+++ overlays/mozilla.nix
@@ -1,1 +1,4 @@
-import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
+import (builtins.fetchTarball {
+  url = "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz";
+  sha256 = "1zybp62zz0h077zm2zmqs2wcg3whg6jqaah9hcl1gv4x8af4zhs6";
+})
