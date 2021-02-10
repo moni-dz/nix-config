354dcb00de19b81560812dfaed5ef176882b4830
fortuneteller2k <lythe1107@gmail.com>
2021-02-10 19:11:28 +0800

    scripts: fix screenshot

--- home/config/neovim.nix
+++ home/config/neovim.nix
@@ -32,8 +32,6 @@
   colorscheme horizon
   let g:lightline = { 'colorscheme' : 'horizon' }
 
-  let g:syntastic_mode_map = { "mode": "passive" }
-
   let g:syntastic_always_populate_loc_list = 1
   let g:syntastic_auto_loc_list = 1
   let g:syntastic_check_on_open = 1
