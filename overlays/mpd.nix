d97a98fd9b994912495858a1d59ee19af99f47ec
fortuneteller2k <lythe1107@gmail.com>
2021-02-26 16:41:33 +0800

    overlays: temporary mpd overlay due to bug

--- overlays/mpd.nix
+++ overlays/mpd.nix
@@ -1,10 +1,10 @@
 final: prev: {
   mpd = prev.mpd.overrideAttrs (old: {
-    src = fetchFromGitHub {
-      owner  = "MusicPlayerDaemon";
-      repo   = "MPD";
-      rev    = "v0.22.6";
-      sha256 = "sha256-CKNw3K/z5UrTIp9ryWq7UaTz768AigaoCIcoJ4iW1j4=";
+    src = prev.fetchFromGitHub {
+      owner = "MusicPlayerDaemon";
+      repo = "MPD";
+      rev = "v0.22.6";
+      sha256 = "sha256-Xu+MxMxR5u++R3lZHe6UQ+mEmRnWbN6173ZX39KS1A8=";
     };
   });
 }
