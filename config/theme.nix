0cea3841b01b949459b0eebbee80b80ef5b2798f
fortuneteller2k <lythe1107@gmail.com>
2021-05-06 09:57:12 +0800

    config: aaa-bbb.nix -> aaa/bbb.nix

--- config/theme.nix
+++ config/theme.nix
@@ -6,9 +6,9 @@ rec {
   primaryColor = "red";
   lightModeEnabled = false;
 
-  # colorscheme to load, see colors-flowtune.nix for a reference impl
+  # colorscheme to load, see colors/flowtune.nix for a reference impl
   colors =
     if !lightModeEnabled
-    then import ./colors-horizon.nix { inherit primaryColor wallpaperPath; }
-    else import ./colors-horizon-light.nix { inherit wallpaperPath; };
+    then import ./colors/horizon.nix { inherit primaryColor wallpaperPath; }
+    else import ./colors/horizon-light.nix { inherit wallpaperPath; };
 }
