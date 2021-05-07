d4f13f713dacb226a3bd39a3fb91fbc61e9022d8
fortuneteller2k <lythe1107@gmail.com>
2021-05-08 06:45:10 +0800

    flake: remove nixpkgs entry in registry since default is set

--- config/nix-conf.nix
+++ config/nix-conf.nix
@@ -5,12 +5,14 @@ rec {
 
   binaryCaches = [
     "https://cache.nixos.org"
+    "https://cache.ngi0.nixos.org/"
     "https://nix-community.cachix.org"
     "https://fortuneteller2k.cachix.org"
   ];
 
   binaryCachePublicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
   ];
@@ -43,7 +45,6 @@ rec {
 
   registry = {
     system.flake = inputs.self;
-    nixpkgs.flake = nixpkgs;
     default.flake = nixpkgs;
     home-manager.flake = inputs.home;
   };
