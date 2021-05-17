1b7a5390c8b4b1bf26d24640cda50388f8c9c03d
fortuneteller2k <lythe1107@gmail.com>
2021-05-18 00:39:49 +0800

    hosts: use systemd-boot

--- hosts/superfluous/configuration.nix
+++ hosts/superfluous/configuration.nix
@@ -36,12 +36,10 @@ in
     loader = {
       efi.canTouchEfiVariables = true;
 
-      grub = {
+      systemd-boot = {
         enable = true;
-        device = "nodev";
-        efiSupport = true;
-        useOSProber = true;
-        gfxmodeEfi = "1366x768";
+        editor = false;
+        consoleMode = "max";
       };
     };
   };
