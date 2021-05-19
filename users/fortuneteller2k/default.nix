8aec675ace9e2cdd537543c34fe3de2fd3e4dec9
fortuneteller2k <lythe1107@gmail.com>
2021-05-19 10:19:55 +0800

    users: migrate htop to new config

--- users/fortuneteller2k/default.nix
+++ users/fortuneteller2k/default.nix
@@ -169,17 +169,27 @@ in
 
     htop = {
       enable = true;
-      detailedCpuTime = true;
-      hideKernelThreads = false;
-      showCpuFrequency = true;
-      showCpuUsage = true;
-      showProgramPath = false;
-      showThreadNames = true;
 
-      meters = {
-        left = [ "AllCPUs" "Memory" "Swap" ];
-        right = [ "Tasks" "LoadAverage" "Uptime" ];
-      };
+      settings = {
+        detailed_cpu_time = true;
+        hide_kernel_threads = false;
+        show_cpu_frequency = true;
+        show_cpu_usage = true;
+        show_program_path = false;
+        show_thread_names = true;
+      }
+      // (with config.lib.htop; leftMeters {
+        AllCPUs = modes.Bar;
+        Memory = modes.Bar;
+        Swap = modes.Bar;
+        Zram = modes.Bar;
+      })
+      // (with config.lib.htop; rightMeters {
+        Tasks = modes.Text;
+        LoadAverage = modes.Text;
+        Uptime = modes.Text;
+        Systemd = modes.Text;
+      });
     };
 
     mako = {
