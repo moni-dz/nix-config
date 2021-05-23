e6dcfa69238a67c9dc9ac1b512c7c1be96704624
fortuneteller2k <lythe1107@gmail.com>
2021-05-24 02:36:50 +0800

    hosts: only support en_US

--- hosts/superfluous/configuration.nix
+++ hosts/superfluous/configuration.nix
@@ -77,7 +77,10 @@ in
     ../../modules/programs/river.nix
   ];
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "en_US.UTF-8/UTF-8" ];
+  };
 
   environment = {
     /*
