f60c797b092f1b41c00ea66024ffce3927d69ac2
fortuneteller2k <lythe1107@gmail.com>
2021-12-26 02:20:55 +0800

    repo: cleanup part 2

--- derivations/spotify-wrapped.nix
+++
@@ -1,5 +0,0 @@
-{ lib, writeShellScriptBin, spotify, spotify-adblock }:
-
-writeShellScriptBin "spotify-wrapped" ''
-  LD_PRELOAD=${spotify-adblock}/lib/spotify-adblock.so ${spotify}/bin/spotify  
-''
