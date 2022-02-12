9bfa7236917baf2fed89806fec843000e4bb6643
fortuneteller2k <lythe1107@gmail.com>
2022-02-12 12:04:03 +0800

    users: remove deprecated config options from dunst

--- users/fortuneteller2k/config/dunst.nix
+++ users/fortuneteller2k/config/dunst.nix
@@ -8,14 +8,14 @@ with colorscheme.colors; {
     word_wrap = "yes";
     horizontal_padding = 8;
     show_indicators = false;
-    frame_width = "2";
+    frame_width = "4";
     format = "<b>%s</b>\\n\\n%b";
     font = "Sarasa Gothic J 10.4";
     frame_color = "#${base0B}";
     separator_color = "auto";
     icon_position = "left";
     max_icon_size = 80;
-    geometry = "330x5-8+25";
+    offset = "8x70";
   };
 
   urgency_low = {
