f53ea4b09a2c8f56c03c56c65d9b9c5f8ea446d4
fortuneteller2k <lythe1107@gmail.com>
2023-06-18 23:27:58 +0800

    starcruiser: use nix.sshServe

--- hosts/starcruiser/configuration.nix
+++ hosts/starcruiser/configuration.nix
@@ -83,6 +83,14 @@
 
   fonts.enableDefaultFonts = false;
 
+  nix.sshServe = {
+    enable = true;
+    protocol = "ssh-ng";
+    keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBlr5SHXali3ttWt8ygyWgCW2usWVsBhXebeyi2XKO2Z lythe1107@gmail.com" ];
+  };
+
+  programs.ssh.pubkeyAcceptedKeyTypes = [ "ssh-ed25519" ];
+
   # https://github.com/nix-community/home-manager/issues/1288#issuecomment-636352427
   programs.sway.enable = true;
 
