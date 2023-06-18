b75922940a8e761dbd007860cf596ea64328eb7d
fortuneteller2k <lythe1107@gmail.com>
2023-06-19 00:34:58 +0800

    starcruiser: samba setup

--- hosts/starcruiser/configuration.nix
+++ hosts/starcruiser/configuration.nix
@@ -83,6 +83,9 @@
 
   fonts.enableDefaultFonts = false;
 
+  networking.firewall.allowedTCPPorts = [ 445 139 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 ];
+
   nix.sshServe = {
     enable = true;
     protocol = "ssh-ng";
@@ -112,6 +115,32 @@
   };
 
   services = {
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        domain = true;
+        hinfo = true;
+        userServices = true;
+        workstation = true;
+      };
+      extraServiceFiles = {
+        smb = ''
+          <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+          <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+          <service-group>
+            <name replace-wildcards="yes">%h</name>
+            <service>
+              <type>_smb._tcp</type>
+              <port>445</port>
+            </service>
+          </service-group>
+        '';
+      };
+    };
+
     greetd = {
       enable = true;
 
@@ -124,6 +153,29 @@
         };
       };
     };
+
+    samba = {
+      enable = true;
+
+      # You will still need to set up the user accounts to begin with:
+      # $ sudo smbpasswd -a yourusername
+
+      # This adds to the [global] section:
+      extraConfig = ''
+        browseable = yes
+        smb encrypt = required
+      '';
+
+      shares = {
+        homes = {
+          browseable = "no";
+          "read only" = "no";
+          "guest ok" = "no";
+        };
+      };
+    };
+
+    samba-wsdd.enable = true;
 
     xserver.videoDrivers = [ "nvidia" ];
   };
