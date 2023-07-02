e0bf1dbbc045cfe3389a6d7e56c27cccb5de6bdb
fortuneteller2k <lythe1107@gmail.com>
2023-07-02 18:27:56 +0800

    agenix: remove hosts that don't exist

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,10 +3,9 @@ let
   moni-darwin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBlr5SHXali3ttWt8ygyWgCW2usWVsBhXebeyi2XKO2Z";
   zero = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf7dkQDloUFN1Hxn/yWrcqMaJiH/jsXUGAAtL9l92xQ";
   starcruiser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrPdqIiTrGqnN6eAhRuGl9ZV2sUz/IR85T3/TzUT4Ol";
-  superfluous = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtfV+2+0M7U6KCnPMiJ63jHp05ucIF1jGvuMNFg6UlT";
 
   users = [ moni-linux moni-darwin zero ];
-  hosts = [ starcruiser superfluous ];
+  hosts = [ starcruiser ];
 in
 {
   "github-token.age".publicKeys = users ++ hosts;
