930743b23d9874b277f43635bffdb0a98578b5b2
fortuneteller2k <lythe1107@gmail.com>
2023-07-03 01:26:30 +0800

    README: update

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,11 +2,11 @@ _:
 
 {
   flake.overlays.default = final: prev: {
-    asitop = prev.callPackage ../derivations/asitop.nix { };
+    asitop = prev.callPackage ./derivations/asitop.nix { };
 
     python3 = prev.python3.override {
       packageOverrides = pfinal: pprev: {
-        pydashing = pfinal.callPackage ../derivations/dashing.nix { };
+        pydashing = pfinal.callPackage ./derivations/dashing.nix { };
       };
     };
 
