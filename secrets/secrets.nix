6529d7ffaa763f6f9c89d339eceaeb266886ddec
fortuneteller2k <lythe1107@gmail.com>
2023-07-02 18:29:53 +0800

    agenix: turn moni into attrset

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,13 @@
 let
-  moni-linux = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf7dkQDloUFN1Hxn/yWrcqMaJiH/jsXUGAAtL9l92xQ";
-  moni-darwin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBlr5SHXali3ttWt8ygyWgCW2usWVsBhXebeyi2XKO2Z";
+  moni = {
+    darwin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBlr5SHXali3ttWt8ygyWgCW2usWVsBhXebeyi2XKO2Z";
+    linux = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf7dkQDloUFN1Hxn/yWrcqMaJiH/jsXUGAAtL9l92xQ";
+  };
+
   zero = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf7dkQDloUFN1Hxn/yWrcqMaJiH/jsXUGAAtL9l92xQ";
   starcruiser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrPdqIiTrGqnN6eAhRuGl9ZV2sUz/IR85T3/TzUT4Ol";
 
-  users = [ moni-linux moni-darwin zero ];
+  users = [ moni.linux moni.darwin zero ];
   hosts = [ starcruiser ];
 in
 {
