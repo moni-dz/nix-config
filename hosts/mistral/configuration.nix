e9f662ac9c68c6d05b7f019589a2679cd5532af5
fortuneteller2k <lythe1107@gmail.com>
2023-11-18 22:22:18 +0800

    hosts/mistral: use recommended PaperMC JVM flags

--- hosts/mistral/configuration.nix
+++ hosts/mistral/configuration.nix
@@ -32,12 +32,32 @@
       openFirewall = true;
 
       jvmOpts = lib.concatStringsSep " " [
-        "-Xmx1800M"
-        "-Xms1G"
+        "-Xms3G"
+        "-Xmx3G"
+        "-XX:+UseG1GC"
+        "-XX:+ParallelRefProcEnabled"
+        "-XX:MaxGCPauseMillis=200"
+        "-XX:+UnlockExperimentalVMOptions"
+        "-XX:+DisableExplicitGC"
+        "-XX:+AlwaysPreTouch"
+        "-XX:G1NewSizePercent=30"
+        "-XX:G1MaxNewSizePercent=40"
+        "-XX:G1HeapRegionSize=8M"
+        "-XX:G1ReservePercent=20"
+        "-XX:G1HeapWastePercent=5"
+        "-XX:G1MixedGCCountTarget=4"
+        "-XX:InitiatingHeapOccupancyPercent=15"
+        "-XX:G1MixedGCLiveThresholdPercent=90"
+        "-XX:G1RSetUpdatingPauseTimePercent=5"
+        "-XX:SurvivorRatio=32"
+        "-XX:+PerfDisableSharedMem"
+        "-XX:MaxTenuringThreshold=1"
+        "-Dusing.aikars.flags=https://mcflags.emc.gs"
+        "-Daikars.new.flags=true"
       ];
 
       serverProperties = {
-        motd = "moni";
+        motd = "hihihi - moni";
         server-port = 43000;
         online-mode = false;
         max-players = 20;
@@ -47,6 +67,15 @@
         "rcon.password" = "longview";
         view-distance = 10;
         spawn-protection = 5;
+      };
+
+      symlinks = {
+        plugins = pkgs.linkFarmFromDrvs "mods" (__attrValues {
+          TabTPS = pkgs.fetchurl {
+            url = "https://cdn.modrinth.com/data/cUhi3iB2/versions/hBGWrGgD/tabtps-spigot-1.3.18.jar";
+            sha512 = "57b5aeea20aced2171a92611e898826a8b080bbbdc741aac3fdd1136e77cdb607afba183860f8e1a3b670f9b3f6febb5def43e51875dbebdddefc2c86dc04c06";
+          };
+        });
       };
     };
   };
