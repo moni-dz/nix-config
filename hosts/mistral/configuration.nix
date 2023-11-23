0ee09fda692b04fb90e658befb96059e0769f471
fortuneteller2k <lythe1107@gmail.com>
2023-11-24 00:41:28 +0800

    hosts/mistral: more plugins

--- hosts/mistral/configuration.nix
+++ hosts/mistral/configuration.nix
@@ -1,7 +1,7 @@
 { inputs, modulesPath, lib, pkgs, ... }:
 
 {
-  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+  imports = lib.optional (__pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
     (modulesPath + "/virtualisation/digital-ocean-config.nix")
   ];
 
@@ -76,27 +76,27 @@
       files = {
         "plugins/worldedit.jar" = pkgs.fetchurl {
           url = "https://dev.bukkit.org/projects/worldedit/files/4793142/download";
-          sha256 = "sha256-8X0+URLekKrp8Ab1io3ejW7zoAQnv26wsjTHhN0B+Ho=";
+          hash = "sha256-8X0+URLekKrp8Ab1io3ejW7zoAQnv26wsjTHhN0B+Ho=";
         };
 
         "plugins/worldprotect.jar" = pkgs.fetchurl {
           url = "https://dev.bukkit.org/projects/worldguard/files/4675318/download";
-          sha256 = "sha256-DATTDAyYwh6CDexvfjgJk0FqKT1+JkaEn23NTv3CQoc=";
+          hash = "sha256-DATTDAyYwh6CDexvfjgJk0FqKT1+JkaEn23NTv3CQoc=";
         };
 
         "plugins/coreprotect.jar" = pkgs.fetchurl {
           url = "https://cdn.modrinth.com/data/Lu3KuzdV/versions/w3P6ufP1/CoreProtect-22.2.jar";
-          sha512 = "070d310bea288f56cdb3442bf80a73f16b1a0d1d82ac877855924b470c483d51f31993c34871063c8041aa1fe68d70a627b9c1fe9def943819b5ec3fcf03f19d";
+          hash = "sha512-Bw0xC+ooj1bNs0Qr+Apz8WsaDR2CrId4VZJLRwxIPVHzGZPDSHEGPIBBqh/mjXCmJ7nB/p3vlDgZtew/zwPxnQ==";
         };
 
         "plugins/spark.jar" = pkgs.fetchurl {
           url = "https://ci.lucko.me/job/spark/396/artifact/spark-bukkit/build/libs/spark-1.10.55-bukkit.jar";
-          sha256 = "sha256-M49tu1FZPRJErpZAJB3QmtuKi98yqfXYZlEftcinGfY=";
+          hash = "sha256-M49tu1FZPRJErpZAJB3QmtuKi98yqfXYZlEftcinGfY=";
         };
 
         "plugins/viaversion.jar" = pkgs.fetchurl {
           url = "https://cdn.modrinth.com/data/P1OZGk5p/versions/DXFf7cQP/ViaVersion-4.9.0-23w45a-SNAPSHOT.jar";
-          sha512 = "abf015d197e7cc037b7a6f12f4354853d0b1d276547f87cc32b3e874ba241de1ebf50f35eca3cd7b41ae26cd4acfaf8f6bba9ba1797394ec4e8260c21bf1e3ff";
+          hash = "sha512-q/AV0ZfnzAN7em8S9DVIU9Cx0nZUf4fMMrPodLokHeHr9Q817KPNe0GuJs1Kz6+Pa7qboXlzlOxOgmDCG/Hj/w==";
         };
 
         "plugins/vault.jar" = pkgs.fetchurl {
@@ -116,17 +116,27 @@
 
         "plugins/essentialx-protect.jar" = pkgs.fetchurl {
           url = "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsXProtect-2.21.0-dev+21-c68b277.jar";
-          sha256 = "sha256-DPNpMvO4rkWoGhsn02Z09h37Fwmdq2/PDmSWLMvxBNk=";
+          hash = "sha256-DPNpMvO4rkWoGhsn02Z09h37Fwmdq2/PDmSWLMvxBNk=";
+        };
+
+        "plugins/deathchest.jar" = pkgs.fetchurl {
+          url = "https://hangarcdn.papermc.io/plugins/CyntrixAlgorithm/DeathChest/versions/2.1.1/PAPER/deathchest.jar";
+          hash = "sha256-djHwoB3AsLBEtegyaWaIBkAwM2WTL8vqtCtJrHduNSw=";
         };
 
         "plugins/chunky.jar" = pkgs.fetchurl {
           url = "https://cdn.modrinth.com/data/fALzjamp/versions/B0xkCkk4/Chunky-1.3.92.jar";
-          sha512 = "63c41849020276a6f0156fc5d11abef77ca53c1ab44a242c97ca522666809c29427805cff38b2de484c7d5e2f3e7eb3eb8a989abcfebbd20419718881e12b00b";
+          hash = "sha512-Y8QYSQICdqbwFW/F0Rq+93ylPBq0SiQsl8pSJmaAnClCeAXP84st5ITH1eLz5+s+uKmJq8/rvSBBlxiIHhKwCw==";
         };
 
         "plugins/luckperms.jar" = pkgs.fetchurl {
           url = "https://download.luckperms.net/1521/bukkit/loader/LuckPerms-Bukkit-5.4.108.jar";
-          sha256 = "sha256-TN7HH/5JiG98xBACfuoJZILsiDxU8WX5laNDS3h+qR4=";
+          hash = "sha256-TN7HH/5JiG98xBACfuoJZILsiDxU8WX5laNDS3h+qR4=";
+        };
+
+        "plugins/placeholderapi.jar" = pkgs.fetchurl {
+          url = "https://hangarcdn.papermc.io/plugins/HelpChat/PlaceholderAPI/versions/2.11.5/PAPER/PlaceholderAPI-2.11.5.jar";
+          hash = "sha256-RDVvvForTFvaqdkwLB1G2o2fpD1JiudaGZPtHy5XhO8=";
         };
 
         "bukkit.yml" = ./config/bukkit.yml;
@@ -134,6 +144,7 @@
         "config/paper-global.yml" = ./config/paper-global.yml;
         "plugins/Essentials/config.yml" = ./config/essentials-config.yml;
         "plugins/WorldEdit/config.yml" = ./config/worldedit-config.yml;
+        "plugins/DeathChest/config.yml" = ./config/deathchest-config.yml;
       };
     };
   };
